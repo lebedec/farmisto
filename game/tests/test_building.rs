2e25bacbb77776fa60f86ccc73364d795f2681bb
Ilya Lebedev <lebedev.games.mail@gmail.com>
2023-01-11 19:57:09 +0300

    try new building algo

--- game/tests/test_building.rs
+++ game/tests/test_building.rs
@@ -3,16 +3,169 @@ use game::building::{
     PLATFORM_SIZE_Y,
 };
 use game::collections::Shared;
+use std::borrow::BorrowMut;
 use std::collections::HashMap;
+use std::time::Instant;
+use std::{mem, ptr};
 
 #[derive(Debug)]
-struct Slot {
-    id: usize,
+struct Shape {
+    pub id: usize,
+    pub contour: bool,
+    pub y: usize,
+    pub max_y: usize,
+    pub rows: Vec<u16>,
+    pub current: u16,
 }
 
-impl Slot {
-    pub fn set_id(&mut self, id: usize) {
-        self.id = id;
+enum Step {
+    Create,
+    Extend { shape: usize },
+    Append { shape: usize },
+    Merge { source: usize, shape: usize },
+}
+
+#[test]
+fn test_something() {
+    println!("value {} {:#016b}", 5, 5);
+    println!("value {} {:#016b}", 0b0101, 0b0101);
+    println!("value {} {:#016b}", 1 << 0 | 1 << 2, 1 << 0 | 1 << 2);
+    println!("value {} {:#016b}", u16::MAX >> 13, u16::MAX >> 13);
+    println!("value {} {:#016b}", 3 & 2, 3 & 2);
+    println!("value {} {:#016b}", 3 & 4, 3 & 4);
+
+    let mut map = [[0u8; 16]; 8];
+    let def_map = r#"
+    . . . . . . . . . . . . . . . .
+    . . # # # # # # # . . . . . . .
+    . . # . . # . . # . # # # . . .
+    . . # . . # . . # . . . # . . .
+    . . # # # # # # # . . . # . . .
+    . . # . . # . . # . # . # . . .
+    . . # # # # # # # . # # # . . . 
+    . . . . . . . . . . . . . . . .
+    "#;
+    for (y, line) in def_map.lines().skip(1).enumerate() {
+        println!("line {}", line);
+        for (x, code) in line.trim().split_whitespace().enumerate() {
+            map[y][x] = match code {
+                "#" => 1,
+                _ => 0,
+            };
+        }
+    }
+
+    let t1 = Instant::now();
+
+    let exterior_shape = Shape {
+        id: 0,
+        contour: false,
+        y: 0,
+        max_y: 0,
+        rows: vec![u16::MAX],
+        current: 0,
+    };
+    let mut shapes: Vec<Shape> = vec![exterior_shape];
+    for y in 1..8 {
+        let mut shape_ptr = None;
+
+        for x in 0..16 {
+            let cell = 1 << (16 - x - 1);
+            let contour = map[y][x] == 1;
+            //println!("Y: {}, X: {}, shapes: {:?}", y, x, shapes);
+            let shape_above = shapes
+                .iter_mut()
+                .position(|shape| {
+                    shape.max_y + 1 == y && shape.rows[shape.rows.len() - 1] & cell == cell
+                })
+                .unwrap();
+
+            if shape_ptr.is_none() {
+                if shapes[shape_above].contour == contour {
+                    shape_ptr = Some(shape_above);
+                } else {
+                    let shape = Shape {
+                        id: shapes.len(),
+                        contour,
+                        y,
+                        max_y: y - 1,
+                        rows: vec![],
+                        current: cell,
+                    };
+                    shapes.push(shape);
+                    shape_ptr = Some(shapes.len() - 1);
+                }
+            } else {
+                if shapes[shape_ptr.unwrap()].contour == contour {
+                    if shapes[shape_above].contour == contour && shape_above != shape_ptr.unwrap() {
+                        //println!("begin merge {} to {:?}", shape_above, shape_ptr);
+
+                        let source = shapes.remove(shape_above);
+                        if shape_ptr.unwrap() > shape_above {
+                            shape_ptr = Some(shape_ptr.unwrap() - 1);
+                        }
+
+                        let shape = &mut shapes[shape_ptr.unwrap()];
+                        let offset = source.y as isize - shape.y as isize;
+                        if offset < 0 {
+                            shape.y = source.y;
+                            let mut rows = vec![0; offset.abs() as usize];
+                            rows.extend(&shape.rows);
+                            shape.rows = rows;
+                        }
+                        for (index, row) in source.rows.into_iter().enumerate() {
+                            shape.rows[index] = shape.rows[index] | row;
+                        }
+                        //println!("end");
+                    } else {
+                        // normal
+                    }
+                } else {
+                    if shapes[shape_above].contour == contour {
+                        shape_ptr = Some(shape_above);
+                    } else {
+                        let shape = Shape {
+                            id: shapes.len(),
+                            contour,
+                            y,
+                            max_y: y - 1,
+                            rows: vec![],
+                            current: cell,
+                        };
+                        shapes.push(shape);
+                        shape_ptr = Some(shapes.len() - 1);
+                    }
+                }
+            }
+
+            let shape = &mut shapes[shape_ptr.unwrap()];
+            shape.current = shape.current | cell;
+        }
+
+        for mut shape in shapes.iter_mut() {
+            if shape.current != 0 {
+                shape.rows.push(shape.current);
+                shape.max_y = y;
+                shape.current = 0;
+            }
+        }
+    }
+
+    println!("elapsed: {}", t1.elapsed().as_secs_f64());
+    println!("shapes: {:?}", shapes.len());
+    for shape in shapes {
+        println!(
+            "shape {} contour: {} interior: {}",
+            shape.id,
+            shape.contour,
+            (shape.id != 0 && !shape.contour)
+        );
+        for row in shape.rows {
+            let row = format!("{:#016b}", row)
+                .replace("0", " .")
+                .replace("1", " #");
+            println!("{}", row);
+        }
     }
 }
 
